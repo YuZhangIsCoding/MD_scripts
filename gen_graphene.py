e0948b5af2e73bec0886e78b977b6b9a90caa083
Yu Zhang <yuzhang23333@gmail.com>
2018-02-22 16:58:40 -0600

    use generator to generate graphene sheets

--- gen_graphene.py
+++ gen_graphene.py
@@ -1,55 +1,63 @@
 #!/Users/yuzhang/anaconda/envs/py3/bin/python
-# This is a python script that generates the coordinate file for graphene sheets
+# This is a python script that generates the coordinate file for graphene sheets.
+# For MD simulations, graphene is usually modeled as an infinite plane. Due to 
+# the periodic boundary conditions, the number of points in each edge must be
+# even. If you want to build a free standing graphene without the boundary
+# limits, please use --real flag.
 
 import argparse
 
 parser = argparse.ArgumentParser(description = 'Specify the size of the electrode')
-parser.add_argument('-s', '--size', nargs = '*', help = 'Specify the approximate size of one graphene sheet')
+parser.add_argument('-s', '--size', nargs = '*', type = float, help = 'Specify the approximate size of one graphene sheet')
 parser.add_argument('-n', '--nlayer', type = int, default = 3, help = 'Specify the number of layers, default is 3')
 parser.add_argument('--bd', type = float, default = 0.142, help = 'Specify the bondlength of C-C bond')
+parser.add_argument('--real', action = 'store_false', help = 'won\'t use periodic boundary conditions')
+parser.add_argument('-v', action = 'store_true', help = 'show details')
 args = parser.parse_args()
 bondlen = args.bd
 
-size = [float(i) for i in args.size]
-l = int(round(size[0]/bondlen*2/6)*2)
-h = int(size[1]/bondlen/3**0.5)*2
-n_layer = args.nlayer
+############ functions #################
+def gf_iter(l, h, dx = 0.5, dy = 0.5*3**0.5):
+    '''generator that returns x and y coordinates'''
+    for i in range(l):
+        if i%2 == 0:
+            x = 3*i*dx+dx
+            carry = 1
+        else:
+            x = 3*i*dx
+            carry = -1
+        for j in range(h):
+            carry = -carry
+            x += dx*carry
+            yield x, dy*j
 
-print('Dimension size of the sheet:', l, h, n_layer)
-print('Real size of the graphene sheet:', bondlen*l*3/2, bondlen*h*3**0.5/2)
+########### main ###################3
+if args.size:
+    size = [float(i) for i in args.size]
+else:
+    size = [float(item) for item in input('Please input the x and y size of the graphene:').split()]
 
-wholepair = [[0]*4 for row in range(l*h)]
+if args.real:
+    l = int(round(size[0]/bondlen*2/6)*2)
+    h = int(size[1]/bondlen/3**0.5)*2
+else:
+    l = int(round(size[0]/bondlen*2/3))
+    h = int(round(2*size[1]/bondlen/3**0.5))
 
-wholepair[0][0]=0
-wholepair[0][1]=0
+if args.v:
+    print('Dimension size of the sheet:', l, h, args.nlayer)
+    print('Real size of the graphene sheet: %.3f, %.3f' %(bondlen*l*3/2, bondlen*h*3**0.5/2))
+    print('Writing to file graphene_sheets.gro ...')
 
-# general frame
-
-for i in range(0,l):
-    if (i%2 == 0):
-        wholepair[h*i][0]=1.5*i
-        wholepair[h*i+1][0]=wholepair[h*i][0]+0.5
-    else:
-        wholepair[h*i][0]=wholepair[h*(i-1)][0]+2
-        wholepair[h*i+1][0]=wholepair[h*i][0]-0.5
-    wholepair[h*i][1]=0
-    wholepair[h*i+1][1]=0.5*3**0.5
-    for j in range(2,h):
-        wholepair[h*i+j][0]=wholepair[h*i+j-2][0]
-        wholepair[h*i+j][1]=wholepair[h*i+j-2][1]+3**0.5
-
-# pure graphene
 grofile = open('graphene_sheets.gro','w')
-grofile.write('%d-layer %dx%d graphene\n' %(n_layer,l,h))
-atoms=l*h*n_layer
-grofile.write('%5d\n' %(atoms))
+grofile.write('%d-layer %dx%d graphene\n' %(args.nlayer, l, h))
+grofile.write('%5d\n' %(l*h*args.nlayer))
 
-for n in range(n_layer):
-    for i in range(0,l*h):
-        wholepair[i][3]='CG'
-        grofile.write("%5d%5s%5s%5d%8.3f%8.3f%8.3f\n" %(n*l*h+i+1,'GPH',wholepair[i][3], n*l*h+i+1,(wholepair[i][0]+n%2)*bondlen, wholepair[i][1]*bondlen, n*0.341))
+count = 0
+for n in range(args.nlayer):
+    for (x, y) in gf_iter(l, h):
+        count += 1
+        grofile.write("%5d%5s%5s%5d%8.3f%8.3f%8.3f\n" %(count, 'GPH', 'CG', count, (x+n%2)*bondlen, y*bondlen, n*0.341))
 
-x=l*3/2*bondlen
-y=h/2*3**0.5*bondlen
-z=(x+y)/2
-grofile.write("%10.5f%10.5f%10.5f\n" %(x, y, z))
+grofile.write("%10.5f%10.5f%10.5f\n" %(l*3/2*bondlen, h/2*3**0.5*bondlen, args.nlayer*0.341))
+grofile.close()
