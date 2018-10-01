78cc73b9ad617133fb5aea4f28d02d024f4680c7
Yu Zhang <yuzhang23333@gmail.com>
2018-10-01 17:23:56 -0500

    prelim code to put water molecules

---
+++ gro2lammps.py
@@ -0,0 +1,105 @@
+#!/Users/yuzhang/anaconda3/bin/python
+# Filename: gro2lammps.py
+# Description:  This is a python script that converts the gromacs file to
+#               LAMMPS file
+# Date:     09-25-2018, created
+
+import argparse
+from gro_common import Gro
+import numpy as np
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-g', '--gro', help = 'grofile')
+parser.add_argument('-ff', '--forcefields', nargs = '*', type = str, 
+                    help = 'force field and supplementary files')
+parser.add_argument('-o', '--output', default = 'data.lammps', help = 'output file')
+parser.add_argument('-onb', '--nbout', default = 'data.lj', 
+                    help = 'output file for lj parameters')
+parser.add_argument('-kcal', '--kcal', action = 'store_true', help = 'Units for energy')
+try:
+    __IPYTHON__
+    args = parser.parse_args([])
+except NameError:
+    args = parser.parse_args()
+
+####
+# for class gro
+gro = Gro()
+gro.load_gro(args.gro)
+gro.load_ff(args.forcefields)
+####
+
+with open(args.output, 'w') as outfile:
+    outfile.write('Generate lammps data file from gromacs file %s and %s\n\n' %(args.gro, args.forcefields))
+
+    outfile.write('%10d atoms\n' %(gro.natoms))
+    if gro.bonds:
+        outfile.write('%10d bonds\n' %(len(gro.bonds)*gro.nmols))
+    if gro.angles:
+        outfile.write('%10d angles\n' %(len(gro.angles)*gro.nmols))
+
+    outfile.write('\n')
+    outfile.write('%10d atom types\n' %(len(gro.atomtypes)))
+    if gro.bondtypes:
+        outfile.write('%10d bond types\n' %(len(gro.bondtypes)//2))
+    if gro.angletypes:
+        outfile.write('%10d angle types\n' %(len(gro.angletypes)//2))
+    outfile.write('\n')
+
+    for i, direction in enumerate('xyz'):
+        outfile.write('%12.6f%12.6f %s %s\n' %(0, 10*gro.box[i], direction+'lo', direction+'hi'))
+
+    outfile.write('\nMasses\n\n')
+    for atom in gro.atomtypes:
+        outfile.write('%5d%12.5f\n' %(gro.atomtypes[atom], gro.mass[atom]))
+
+    if gro.bondtypes:
+        outfile.write('\nBond Coeffs\n\n')
+        appeared = set()
+        for bond in gro.bondtypes:
+            if gro.bondtypes[bond][0] not in appeared:
+                appeared.add(gro.bondtypes[bond][0])
+                outfile.write('%5d%8.3f%8.3f\n' %(gro.bondtypes[bond][0], gro.bondtypes[bond][1], gro.bondtypes[bond][2]))
+
+    if gro.angletypes:
+        outfile.write('\nAngle Coeffs\n\n')
+        appeared = set()
+        for angle in gro.angletypes:
+            if gro.angletypes[angle][0] not in appeared:
+                appeared.add(gro.angletypes[angle][0])
+                outfile.write('%5d%8.3f%8.3f\n' %(gro.angletypes[angle][0], gro.angletypes[angle][1], gro.angletypes[angle][2]))
+
+    outfile.write('\nAtoms\n\n')
+    for i, item in enumerate(gro.info):
+        chargeinx = i%len(gro.mq)
+        outfile.write('%5d%5d%5d%12.5f%8.3f%8.3f%8.3f\n'
+                %(i+1, i//len(gro.mq)+1, gro.atomtypes[item[2]],
+                    gro.mq[i%len(gro.mq)][0], item[-3]*10, item[-2]*10, item[-1]*10))
+
+    outfile.write('\nBonds\n\n')
+    num_per_mol = gro.natoms//gro.nmols
+    cnt = 1
+    for bond in gro.bonds:
+        for i in range(gro.nmols):
+            seq = gro.bondtypes[tuple([gro.info[_-1][2] for _ in bond])][0]
+            outfile.write('{:10d}{:10d}{:10d}{:10d}\n'.format(cnt, seq, *[_+i*num_per_mol for _ in bond]))
+            cnt += 1
+
+    outfile.write('\nAngles\n\n')
+    cnt = 1
+    for angle in gro.angles:
+        for i in range(gro.nmols):
+            seq = gro.angletypes[tuple([gro.info[_-1][2] for _ in angle])][0]
+            outfile.write('{:10d}{:10d}{:10d}{:10d}{:10d}\n'.format(cnt, seq, *[_+i*num_per_mol for _ in angle]))
+
+                
+## add pair_coeffs
+with open(args.nbout, 'w') as outfile:
+    outfile.write('#Pair Coeffs\n\n')
+    typelist = list(gro.atomtypes.keys())
+    for i in range(len(typelist)):
+        for j in range(i, len(typelist)):
+            epsilon = np.sqrt(gro.lj[typelist[i]][0]*gro.lj[typelist[j]][0])
+            sigma = (gro.lj[typelist[i]][1]+gro.lj[typelist[j]][1])/2
+            outfile.write('pair_coeff\t%d\t%d%12.6f%12.6f\n' 
+                    %(gro.atomtypes[typelist[i]], gro.atomtypes[typelist[j]], epsilon, sigma))
--- gro_common.py
+++ gro_common.py
@@ -7,14 +7,30 @@
 
 class Gro(object):
     def __init__(self):
+        '''
+        info:   list of parsed line in .gro file, based on gro file format, 
+                including: residue number, residue name, atom name, 
+                atom number position(x, y, z)
+        atomtypes:  dictionary of atomtype and its corresponding index
+        mq:     list of atom mass and charge
+        mass:   dictionary of atomtypes and their corresponding mass
+        lj:     dictionary of atomtypes and their corresponding lj parameters
+                the order is [sigma, epsilon]
+        '''
         self.box = None
         self.natoms = None
         self.nmols = None
         self.info = []
         self.atomtypes = {}
-        self.itp = []
+        self.mq = []
         self.mass = {}
         self.lj = {}
+        self.bondtypes = {}
+        self.angletypes = {}
+        self.dihedraltypes = {}
+        self.bonds = []
+        self.angles = []
+        self.dihedrals = []
     def load_gro(self, filename):
         '''read *.gro file, save atom info and box size'''
         with open(filename) as myfile:
@@ -29,9 +45,40 @@ class Gro(object):
                 self.box.append(0)
         self.check_itp_gro()
         self.get_atomtypes()
-    def load_itp(self, filename):
+    def load_ff(self, filenames, nonbond = None):
+        '''Read in itp file, and load mass and charge infomation such as
+        molar mass, atomic charge, bonds, angles, dihedrals, etc.'''
+        fftypes = {}
+#        sessions = ['atoms', 'bondtypes', 'angletypes', 'nonbond']
+        for filename in filenames:
+            with open(filename) as myfile:
+                for line in myfile:
+                    if '[' in line and ']' in line:
+                        if 'atoms' in line:
+                            fftypes['mq'] = filename
+                        elif 'bondtypes' in line:
+                            fftypes['bondtypes'] = filename
+                        elif 'angletypes' in line:
+                            fftypes['angletypes'] = filename
+                        elif 'dihedraltypes' in line:
+                            fftypes['dihedral'] = filename
+                        elif 'atomtypes' in line:
+                            fftypes['nonbond'] = filename
+                        elif 'bonds' in line:
+                            fftypes['bonds'] = filename
+                        elif 'angles' in line:
+                            fftypes['angles'] = filename
+        self.load_mq(fftypes['mq'])
+        self.load_bondtypes(fftypes['bondtypes'])
+        self.load_bonds(fftypes['bonds'])
+        self.load_angletypes(fftypes['angletypes'])
+        self.load_angles(fftypes['angles'])
+        self.load_nonbonds(fftypes['nonbond'])
+        self.check_itp_gro()
+        self.nmols = self.natoms//len(self.mq)
+    def load_mq(self, filename):
         '''Read corresponding *.itp file and save charge and molar mass
-        into self.itp and self.mass.
+        into self.mq and self.mass.
         Note that one atom type could have different charges but could
         only have one molar mass'''
         with open(filename) as myfile:
@@ -49,11 +96,90 @@ class Gro(object):
                     temp = line.split()
                     if temp[1] not in self.mass:
                         self.mass[temp[1]] = float(temp[-1])
-                    self.itp.append([float(_) for _ in temp[-2:]])
-        self.check_itp_gro()
-    def load_nonbond(self, filename):
+                    self.mq.append([float(_) for _ in temp[-2:]])
+    def load_bondtypes(self, filename, kcal = True):
+        with open(filename) as myfile:
+            cnt = 1
+            flag = False
+            for line in myfile:
+                if line.strip() == '' or line.strip()[0] in [';', '#']:
+                    continue
+                if '[' in line and ']' in line:
+                    if 'bondtypes' in line:
+                        flag = True
+                        continue
+                    elif flag:
+                        break
+                if flag:
+                    temp = line.split()
+                    k = float(temp[-1])
+                    b = float(temp[-2]) #angstrom
+                    if not kcal:
+                        k /= 100*4.184*2 # kJ to kCal/A^2
+                        b *= 10 # nm to angstrom
+                    self.bondtypes[(temp[0], temp[1])] = [cnt, k, b]
+                    self.bondtypes[(temp[1], temp[0])] = self.bondtypes[(temp[0], temp[1])]
+                    cnt += 1
+
+    def load_bonds(self, filename):
+        with open(filename) as myfile:
+            flag = False
+            for line in myfile:
+                if line.strip() == '' or line.strip()[0] in [';', '#']:
+                    continue
+                if '[' in line and ']' in line:
+                    if 'bonds' in line:
+                        flag = True
+                        continue
+                    elif flag:
+                        break
+                if flag:
+                    self.bonds.append([int(_) for _ in line.split()[:2]])
+                    
+    def load_angletypes(self, filename, kcal = True):
+        with open(filename) as myfile:
+            cnt = 1
+            flag = False
+            for line in myfile:
+                if line.strip() == '' or line.strip()[0] in [';', '#']:
+                    continue
+                if '[' in line and ']' in line:
+                    if 'angletypes' in line:
+                        flag = True
+                        continue
+                    elif flag:
+                        break
+                if flag:
+                    temp = line.split()
+                    theta = float(temp[-2]) 
+                    k = float(temp[-1])
+                    if not kcal:
+                        k /= 4.184*2 # kJ/rad^2 to kCal/rad^2
+                    self.angletypes[(temp[0], temp[1], temp[2])] = [cnt, k, theta]
+                    self.angletypes[(temp[2], temp[0], temp[0])] = self.angletypes[(temp[0], temp[1], temp[2])]
+                    cnt += 1
+    def load_angles(self, filename):
+        with open(filename) as myfile:
+            flag = False
+            for line in myfile:
+                if line.strip() == '' or line.strip()[0] in [';', '#']:
+                    continue
+                if '[' in line and ']' in line:
+                    if 'angles' in line:
+                        flag = True
+                        continue
+                    elif flag:
+                        break
+                if flag:
+                    self.angles.append([int(_) for _ in line.split()[:3]])
+    def load_dihedrals(self):
+        pass
+    def load_nonbonds(self, filename, kcal = True):
         '''Read gromacs topology file and find the Lennard-Jones parameter
         and write it with atomtypes'''
+        # if the input nonbonded file is an itp file, need to change the units
+        if filename.endswith('.itp') or filename.endswith('.top'):
+            kcal = False
         with open(filename) as myfile:
             flag = False
             for line in myfile:
@@ -68,7 +194,12 @@ class Gro(object):
                 if flag:
                     temp = line.split()
                     if temp[0] in self.atomtypes:
-                        self.lj[temp[0]] = [float(_) for _ in temp[-2:]]
+                        sigma = float(temp[-2])
+                        epsilon = float(temp[-1])
+                        if not kcal:
+                            sigma *= 10
+                            epsilon /= 4.184
+                        self.lj[temp[0]] = [epsilon, sigma]
     def get_atomtypes(self):
         # Note if we want to deal with multiple input gro
         # file, probably need to define a unique atomtype
@@ -81,12 +212,13 @@ class Gro(object):
     def check_itp_gro(self):
         '''Check if the number of atoms in gro file is a multiple of
         the number of atoms in itp file'''
-        if self.natoms and self.itp and self.natoms%len(self.itp):
+        if self.natoms and self.mq and self.natoms%len(self.mq):
             raise Exception('gro file does not match itp file!')
     @staticmethod
     def read_line(string):
+        '''gro file format '%5d%5s%5s%5d%8.3f%8.3f%8.3f'
+        '''
         results = []
-        #gro file format '%5d%5s%5s%5d%8.3f%8.3f%8.3f'
         for i in range(4):
             results.append(string[i*5:(i+1)*5].strip())
         results.extend([float(_) for _ in string[20:44].split()])
