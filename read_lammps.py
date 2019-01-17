1391d23e30cc7b8c0ccfa57042eeddbfce5cf29d
Yu Zhang <yuzhang23333@gmail.com>
2019-01-17 17:46:49 -0600

    gro_split

--- read_lammps.py
+++ read_lammps.py
@@ -4,6 +4,7 @@
 #               infomations from lammps simulation, such as charge, atoms
 #               coordinates, etc.
 # Dates:        09-29-2018  Created
+#               01-16-2018  Record charges for multiple atoms
 
 import argparse
 import numpy as np
@@ -28,24 +29,27 @@ def read_charge(args):
     with open(args.input) as myfile:
         for line in myfile:
             if 'TIMESTEP' in line:
-                time_flag = True
+                time_flag = True # flag to record current time
+                charge_flag = False # flag to record current charge
                 continue
             if time_flag:
                 time = int(line)
                 if time == time_pre:
+                    # make sure same timestep won't be record twice
                     time = None
                 else:
                     time_pre = time
+                    charge.append([time])
                 time_flag = False
                 continue
             if time is not None and 'ITEM: ATOMS id type q' in line:
+                # after this line, the charge will be appended to the list
                 charge_flag = True
                 continue
             if charge_flag:
-                charge.append((time, float(line.split()[-1])))
-                charge_flag = False
+                charge[-1].append(float(line.split()[-1]))
     charge = np.array(charge)
-    np.savetxt(args.output, charge, fmt = ['%10d', '%12.3f'])
+    np.savetxt(args.output, charge, fmt = ['%10d']+['%12.3f']*(charge.shape[1]-1))
 
 if args.choice == 'charge':
     read_charge(args)
